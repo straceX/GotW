16bf52fd1816acb59e8077d805f5666d5afaf3a6
0x80 <netsecdeveloper@gmail.com>
2017-03-20 23:09:30 +0300

    Refactoring
    Refactoring

--- GotW/GotW1.cpp
+++ GotW/GotW1.cpp
@@ -6,10 +6,10 @@ int main()
 {
 
     int ival1 = 13;
-    int ival2{14}; //C++11
+    int ival2{14}; // C++11
     int ival3(15);
     int ival4;
-    int ival5();
+    int ival5(); // it's a function declaration
 
     std::cout << ival1 <<std::endl;
     std::cout << ival2 <<std::endl;
