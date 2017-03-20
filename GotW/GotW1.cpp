acc002411e22c12e152450bd556507883a587ad4
int 0x80 <netsecdeveloper@gmail.com>
2017-03-20 15:34:47 +0300

    Solving of Variable Initialization problem. (How many ways are there to initialize variables?)
    http://www.gotw.ca/gotw/001.htm

---
+++ GotW/GotW1.cpp
@@ -0,0 +1,23 @@
+/* g++ GotW1.cpp -omain -std=c++11 */
+
+#include <iostream>
+
+int main()
+{
+
+    int ival1 = 13;
+    int ival2{14}; //C++11
+    int ival3(15);
+    int ival4;
+    int ival5();
+
+    std::cout << ival1 <<std::endl;
+    std::cout << ival2 <<std::endl;
+    std::cout << ival3 <<std::endl;
+    std::cout << ival4 <<std::endl;
+    std::cout << ival5 <<std::endl;
+
+    return 0;
+}
+
+
